1b58fae1f3cc08c5415d2846e498e625a9e43a19
Willian <108596048+DvWillz@users.noreply.github.com>
2026-03-05 16:30:33 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from random import choice
-characteres = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%¨&&*()'
-senha = ''
-quantidade = int(input("digite a quantidade de characteres de sua senha: "))
-
-for i in range(quantidade):
-    senha += choice(characteres)
-print(f"Sua senha foi gerada: {senha}")
